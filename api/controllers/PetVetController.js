fa362c8817eb55f48c50084e8a8a16782a5d2a89
Joao Paulo <juaumlol@gmail.com>
2014-12-19 15:31:11 -0200

    distancia, detail api

--- api/controllers/PetVetController.js
+++ api/controllers/PetVetController.js
@@ -6,6 +6,75 @@
  */
 
 module.exports = {
-	
+  index: function(req, res) {
+    var id = req.param('id'),
+        lat = req.param("lat"), 
+        lng = req.param("lng");
+    if(id == undefined) {
+      res.status(400).json({err:"id not specified"});  
+    }else{
+      if(lat === undefined || lng === undefined){
+        PetVet.findOne(id).exec(function(err, petvet){
+          if(err){
+            res.status(400).json({err:"invalid id"});
+          }else{
+            if(!petvet) res.status(400).json({err:"not found"});
+            else {
+              delete petvet.foursquareRef;
+              delete petvet.createdAt;
+              delete petvet.updatedAt;
+              res.status(200).json(petvet);
+            }
+          }
+        });
+      }else{
+        lat = parseFloat(lat);
+        lng = parseFloat(lng);          
+        PetVet.native(function(err, collection) {
+          if(err) res.status(400).json({err:"error getting model"});
+          else{
+            collection.aggregate(
+              [ 
+                {
+                  '$geoNear': {
+                    near :  [lng, lat],
+                    spherical : true,
+                    distanceField: "distance",
+                    query: {'_id': require('mongodb').ObjectID(id)},
+                    distanceMultiplier: 6371
+                  }
+                },
+                {
+                  '$project': {
+                    _id: false,
+                    id: '$_id',
+                    distance: 1,
+                    type: 1,
+                    name: 1,
+                    loc: 1,
+                    address: 1,
+                    image: 1,
+                    phones: 1,
+                    email: 1,
+                    website: 1                
+                  }
+                }
+              ],          
+              function(err, result){
+                if (err) res.status(400).json({err:"error searching"});
+                else{
+                  if(result.length == 0) {
+                    res.status(400).json({err:"not found"});
+                  }else{
+                    res.status(200).json(result[0]);
+                  }                  
+                }          
+              }
+            );                
+          }
+        }); 
+      }
+    }    
+  }
 };
 
