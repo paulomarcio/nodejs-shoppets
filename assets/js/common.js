0afc5e2c84fa3bb3355d1ecea207b86fd686509c
Paulo Márcio <paulom.matias@gmail.com>
2015-01-19 16:57:55 -0200

    Acertando busca

--- assets/js/common.js
+++ assets/js/common.js
@@ -43,7 +43,7 @@ function calcRoute(start, destination) {
 
 function geoCodeByAddress(endereco) {
     geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
+    geocoder.geocode( { 'address': endereco}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             return results[0].geometry.location;
         } else {
